f140c3ce9c32ef53bd0510a7e0a8ba9a3cafd058
ATSU <mifutsua@gmail.com>
2023-05-13 21:25:09 +0900

    update

---
+++ src/components/Card.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="card">
+        <div class="card-image-wrapper">
+            <img :src="imageUrl" :alt="title" class="card-image" />
+        </div>
+        <div class="card-content">
+            <h3 class="card-title">{{ title }}</h3>
+            <p class="card-description">{{ description }}</p>
+            <button class="start-button">Start</button>
+        </div>
+    </div>
+</template>
+  
+  
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+        imageUrl: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+    },
+};
+</script>
+  
---
+++ src/components/List.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="list">
+        <h2>Choose the Dish</h2>
+        <Card v-for="(item, index) in items" :key="index" :title="item.title" :imageUrl="item.imageUrl"
+            :description="item.description" />
+    </div>
+</template>
+  
+<script>
+import Card from "./Card.vue";
+
+export default {
+    components: {
+        Card,
+    },
+    data() {
+        return {
+            items: [
+                {
+                    title: "Item 1",
+                    imageUrl: "https://picsum.photos/300/300",
+                    description: "This is the description for Item 1.",
+                },
+                {
+                    title: "Item 2",
+                    imageUrl: "https://picsum.photos/300/300",
+                    description: "This is the description for Item 2.",
+                },
+                {
+                    title: "Item 3",
+                    imageUrl: "https://picsum.photos/300/300",
+                    description: "This is the description for Item 3.",
+                },
+            ],
+        };
+    },
+};
+</script>
+
+  
