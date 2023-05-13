093e197aa353629e48e4135b20f9b862d7ad8f74
ATSU <mifutsua@gmail.com>
2023-05-13 19:55:51 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import autoprefixer from 'autoprefixer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer
+      ]
+    }
+  },
   build: {
-    assetsDir: "static",
+    assetsDir: 'static',
   },
 });
